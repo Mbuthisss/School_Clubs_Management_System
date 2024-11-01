c43ab183874618443b38b367334e098e7597c0c0
Ryan Mbuthia <ryan.mbuthia02@gmail.com>
2024-11-01 20:59:15 +0300

    Version 1.5

--- app/src/main/java/com/example/schoolclubsmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/schoolclubsmanagementsystem/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.schoolclubsmanagementsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.schoolclubsmanagementsystem.activities.LoginActivity;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.example.schoolclubsmanagementsystem.user.Authentication;
 
 public class MainActivity extends AppCompatActivity {
 
     private Authentication auth;
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +38,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Initialize Firebase
-        FirebaseApp.initializeApp(this);
+
+        drawerLayout = findViewById(R.id.main);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.navigationView);
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home){
+                    Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.clubs_list) {
+                    Toast.makeText(MainActivity.this, "Clubs List Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.my_clubs) {
+                    Toast.makeText(MainActivity.this, "My Clubs Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.profile) {
+                    Toast.makeText(MainActivity.this, "Profile Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.member_app) {
+                    Toast.makeText(MainActivity.this, "Member Applications Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.nots) {
+                    Toast.makeText(MainActivity.this, "Notifications Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.payment) {
+                    Toast.makeText(MainActivity.this, "Payments Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.help) {
+                    Toast.makeText(MainActivity.this, "Help Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.feedback) {
+                    Toast.makeText(MainActivity.this, "Feedback Clicked", Toast.LENGTH_SHORT).show();
+
+                } else if (itemId == R.id.logout) {
+                    Toast.makeText(MainActivity.this, "Logout Clicked", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.close();
+
+                return false;
+            }
+        });
+
 
 /*
         // Initialize Authentication
---
+++ app/src/main/java/com/example/schoolclubsmanagementsystem/activities/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.schoolclubsmanagementsystem.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.schoolclubsmanagementsystem.MainActivity;
+import com.example.schoolclubsmanagementsystem.R;
+import com.google.firebase.FirebaseApp;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
